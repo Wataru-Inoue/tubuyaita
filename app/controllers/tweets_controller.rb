81f450f19f1e524cb5aa35c958878d7bd9ca885c
Wataru Inoue <wataru.inoue01@gmail.com>
2017-04-28 13:27:59 +0000

    confirm

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+  before_action :set_blog, only: [:edit, :update, :destroy ]
+  
   def index
     @tweets = Tweet.all
   end
@@ -10,31 +12,36 @@ class TweetsController < ApplicationController
   def create
     @tweet = Tweet.new(tweets_params)
     if @tweet.save
-       redirect_to tweets_path, notice:"tweetを投稿しました！"
+       redirect_to tweets_path, notice:"つぶやきました！"
     else
       render 'new'
     end
   end
   
   def edit
-    @tweet = Tweet.find(params[:id])
   end
 
   def update
-    @tweet = Tweet.find(params[:id])
     @tweet.update(tweet_params)
-    redirect_to tweets_path, notice:"tweetを編集しました！"
+    redirect_to tweets_path, notice:"つぶやきを編集しました！"
   end
   
   def destroy
-    @tweet = Tweet.find(params[:id])
     @tweet.destroy
-    redirect_to tweets_path, notice:"tweetを削除しました！"
+    redirect_to tweets_path, notice:"つぶやきを削除しました！"
+  end
+  
+  def confirm
+    @tweet = Tweet.new(tweets_params)
   end
   
   private
     def tweets_params
       params.require(:tweet).permit(:content)
+    end
+    
+    def set_tweet
+      @tweet = Tweet.find(params[:id])
     end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :tweets, only: [:index, :new, :create, :edit, :update, :destroy] 
+  resources :tweets, only: [:index, :new, :create, :edit, :update, :destroy] do
+    collection do
+        post :confirm
+    end
+  end
 
 end
